fe24d75d03cde26ea25b64a97a78318a80502563
Korobokkk <bbb754748@gmail.com>
2025-09-26 23:47:39 +0300

    The array creation logic has been changed

--- 3-heap and quick sor/Source.cpp
+++ 3-heap and quick sor/Source.cpp
@@ -86,7 +86,7 @@ void filling_an_array( std::vector<int> &arr, char type_of_reading, int n, int q
 		std::cout << "n - " << n << ", q - " << q << ", w - " << w << "\n\n" << "Select an array generation method:\n    1.-Pseudo-random generation\n    2- Ascending generation\n    3- Descending generation\n";//debug
 		int choose;
 		std::cin >> choose;
-		void (*function_selection)(int, int&, int&) {};
+		//void (*function_selection)(int, int&, int&) {};
 		if (choose < 1 || choose>3)
 		{
 			std::cout << "Incorrect input num!!!";
@@ -94,37 +94,134 @@ void filling_an_array( std::vector<int> &arr, char type_of_reading, int n, int q
 		}
 		else if (choose == 1)
 		{
-			function_selection = pseudo_random;
+			for (int i = 0; i < n; ++i)
+			{
+				int tmp = std::rand() % (w - q + 1) + q; //add 1 for tmp from [q, w]
+				arr.push_back(tmp);
+				std::cout << tmp << " ";
+				/*if (i % 10 == 9)
+				{
+					std::cout << "\n";
+				}*/
+			}
 		}
 		else if (choose == 2)
 		{
-			function_selection = ascending_generation;
+			arr.push_back(q);
+			std::cout << q++<< " ";
+			
+
+			for (int i = 1; i < n; ++i)
+			{
+				if (q == w)
+				{
+					for (i; i < n; ++i)//maybe shiza
+					{
+						std::cout << q << " ";
+						arr.push_back(q);
+					}
+					break;
+					
+				}
+				else
+				{
+					std::cout << q << " ";
+					arr.push_back(q++);				
+				}
+			}
 		}
 		else {
-			function_selection = descending_generation;
-		}
-
-		std::cout << "You generated array:\n";		
-		for (int i = 0; i < n; i++)
-		{
-			int tmp = std::rand() % (w-q+1) + q;
-			arr.push_back(tmp);
-			std::cout << tmp << " ";
-			if (i % 10 == 9)
+			arr.push_back(w);
+			std::cout << w-- << " ";
+			for (int i = n-2; i >= 0; --i)
 			{
-				std::cout << "\n";
+				if (w == q)
+				{
+					for (i; i >= 0; --i)//maybe shiza
+					{
+						std::cout << w << " ";
+						arr.push_back(w);
+					}
+					break;
+				}
+				else
+				{
+					std::cout << w << " ";
+					arr.push_back(w--);
+				}
 			}
-			function_selection(tmp, q, w);
-		}
-		std::cout << std::endl;
+		}//NEED MORE REFACT!!!!!
+
+		std::cout << "\n";
 	}
 	else//for .exe file
 	{
 		//need write
 	}
 }
+//void devive(std::vector<int>, int i, int j, int i1)
+//{
+//
+//}
+//
+//std::vector<int> quick_sortv2(std::vector<int> arr, int i, int j)
+//{
+//	int i1;
+//	if (i < j)
+//	{
+//		devive(arr, i, j);
+//		quick_sortv2(arr, i, i1 - 1);
+//		quick_sortv2(arr, i1 + 1, j);
+//	}
+//
+//}
 
-std::vector<int> quick_sort(std::vector<int> arr, int n)
+//std::vector<int> quick_sort(std::vector<int> arr, int min_index_value, int n)
+//{
+//	if (min_index_value >= n)
+//	{
+//		return arr;
+//	}
+//
+//	/*std::vector<int> less_or_equal_than;
+//	std::vector<int> more_than;*/
+//	
+//	//int index_last_elem = (int)arr.size() - 1;
+//	int index_last_elem = n- 1;
+//
+//	/*for (int i = 0; i < index_last_elem; i++)
+//	{
+//		if (arr[i] <= arr[index_last_elem])
+//		{
+//			less_or_equal_than.push_back(arr[i]);
+//		}
+//		else
+//		{
+//			more_than.push_back(arr[i]);
+//		}
+//	}*/
+//
+//	for (int i = 0; i < index_last_elem; i++)
+//	{
+//		if (arr[i] >= arr[index_last_elem])
+//		{
+//			arr.push_back(arr[i]);
+//			arr.erase(arr.begin()+ i);
+//			i--;
+//			index_last_elem--;
+//		}
+//	}
+//	quick_sort(arr, 0, index_last_elem);
+//	quick_sort(arr, index_last_elem + 1, n);
+//	
+//
+//	less.push_back(arr[index_last_elem]);
+//	std::copy(more.begin(), more.end(),std::back_inserter(less));
+//
+//	return less;//need refuct with create vector
+//}
+
+std::vector<int> quick_sort(std::vector<int> arr, int min_value, int n)
 {
 	if (arr.size() <= 1)
 	{
@@ -133,8 +230,8 @@ std::vector<int> quick_sort(std::vector<int> arr, int n)
 
 	std::vector<int> less_or_equal_than;
 	std::vector<int> more_than;
-	
-	int index_last_elem = arr.size() - 1;
+
+	int index_last_elem = (int)arr.size() - 1;
 
 	for (int i = 0; i < index_last_elem; i++)
 	{
@@ -147,14 +244,16 @@ std::vector<int> quick_sort(std::vector<int> arr, int n)
 			more_than.push_back(arr[i]);
 		}
 	}
-	std::vector<int> less(quick_sort(less_or_equal_than, n));
-	std::vector<int> more(quick_sort(more_than, n));
-	
+	std::vector<int> less(quick_sort(less_or_equal_than, min_value, n));
+	std::vector<int> more(quick_sort(more_than,min_value, n));
+
 	less.push_back(arr[index_last_elem]);
-	std::copy(more.begin(), more.end(),std::back_inserter(less));
+	std::copy(more.begin(), more.end(), std::back_inserter(less));
 
 	return less;//need refuct with create vector
 }
+
+
 
 // 3-heapsort function
 
@@ -192,7 +291,7 @@ void shift_down(std::vector<int>& arr, int i, int n)
 
 
 
-std::vector<int> three_heap_sort(std::vector<int> arr, int n)//added n in input args and in QuickSort 
+std::vector<int> three_heap_sort(std::vector<int> arr, int i, int n)//added n in input args and in QuickSort 
 {
 	
 	for (int i = n-2 / 3; i >= 0; i--)
@@ -225,19 +324,21 @@ void out_array_sort_res(std::vector<int> arr)
 
 }
 
-bool sort_selection(std::vector<int> arr,int n)
+bool sort_selection(std::vector<int> arr, int min_index_elemint, int n)
 {
 	char select;
 	std::cout << "Sorting selection:\n    1-for Quicksort\n    2-for 3-heap sort\n    3-for exit\n";
 	std::cin >> select;
-	std::vector<int>(*select_sort)(std::vector<int> arr, int n);
+	std::vector<int>(*select_sort)(std::vector<int> arr,int min_index_elem, int n);
 
 	if (select == '1')
 	{
 		select_sort = quick_sort;
 	}
 	else if(select == '2')
-	{
+	{/*
+		std::cout << "tmp not working\n";
+		return false;*/
 		select_sort = three_heap_sort;
 	}
 	else
@@ -247,12 +348,12 @@ bool sort_selection(std::vector<int> arr,int n)
 	}
 
 	auto wall_start = std::chrono::high_resolution_clock::now();
-	std::vector<int> sort_res(select_sort(arr, n));
+	std::vector<int> sort_res(select_sort(arr,0, n));
 	auto wall_stop = std::chrono::high_resolution_clock::now();
 	auto wall_time = std::chrono::duration_cast<std::chrono::milliseconds>(wall_stop - wall_start).count();
 	
 	bool see_result;
-	std::cout << "Do you want to see the sorted array?\n";
+	std::cout << "Do you want to see the sorted array?\n    1-YES\n    0-NO\n";
 	std::cin >> see_result;
 	if (see_result)
 	{
@@ -274,7 +375,7 @@ int main()
 	filling_an_array(arr, type_of_reading, n, q, w);
 	
 	//work with arr
-	while (sort_selection(arr, n))
+	while (sort_selection(arr,0, n))
 	{
 	}
 	return 0;
